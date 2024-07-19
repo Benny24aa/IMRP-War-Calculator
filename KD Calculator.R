aa9bc1056b4b36de0916fdbd5b8445635f022baa
Ben Harley <harleyb101010@gmail.com>
2024-07-19 18:38:14 +0100

    added factions to k/d calculation

--- KD Calculator.R
+++ KD Calculator.R
@@ -18,24 +18,28 @@ IMRP_Data <- read_xlsx(Data_Location, 3) ### Load in data
 #### Killer Data Analytics
 
 IMRP_Data_Cleaned_Kills <- IMRP_Data |>
-  select (`Killer Name`, `Killed Name`, Reason, `Date Killed`, `Team Kill`,`Date Killed`)|> # Reducing size of dataset to speed up process
+  select (`Killer Name`, `Killed Name`, Reason, `Date Killed`, `Team Kill`,`Date Killed`, `Killer Faction Id`)|> # Reducing size of dataset to speed up process
   filter(`Team Kill` == FALSE)|>
   mutate(DATE = as.Date(`Date Killed`), format = "%d/%m/%Y")|>
-  group_by(`Killer Name`, `DATE`)|>
+  group_by(`Killer Name`, `Killer Faction Id`, `DATE`)|>
   summarise(count=n(), .groups = 'drop')|>
   rename('Player Name' =`Killer Name` )|>
-  rename('Deaths' = count)
+  rename('Faction' =`Killer Faction Id` )|>
+  rename('Kills' = count)
 
 #### Killed Data Analytics
 IMRP_Data_Cleaned_Deaths <- IMRP_Data |>
-  select (`Killer Name`, `Killed Name`, Reason, `Date Killed`, `Team Kill`,`Date Killed`)|> # Reducing size of dataset to speed up process
+  select (`Killer Name`, `Killed Name`, Reason, `Date Killed`, `Team Kill`,`Date Killed`, `Killed Faction Id`)|> # Reducing size of dataset to speed up process
   filter(`Team Kill` == FALSE)|>
   mutate(DATE = as.Date(`Date Killed`), format = "%d/%m/%Y")|>
-  group_by(`Killed Name`, `DATE`)|>
+  group_by(`Killed Name`, `Killed Faction Id`, `DATE`)|>
   summarise(count=n(), .groups = 'drop')|>
   rename('Player Name' =`Killed Name` ) |>
-  rename('Kills' = count)
+  rename('Deaths' = count)|>
+  rename('Faction' =`Killed Faction Id`)
 
 #### Merging Data for Analytics
   
  Merged <- merge(IMRP_Data_Cleaned_Deaths, IMRP_Data_Cleaned_Kills)
+ 
+write.csv(Merged,"C:/Users/harle/OneDrive/Desktop/IMRP 2024 Improved  War Census/Morgan vs Tattaglia Main War Files/last update/KD.csv")
